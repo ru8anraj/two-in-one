ced8d01a135160c087a50f6cd3b0f25ec92c2261
rubanraj <rubanraj.r07@wipro.com>
2019-09-06 09:11:24 +0100

    first commit

---
+++ src/main/java/com/ru8anraj/twoinone/TwoInOneApplication.java
@@ -0,0 +1,13 @@
+package com.ru8anraj.twoinone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoInOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoInOneApplication.class, args);
+	}
+
+}
